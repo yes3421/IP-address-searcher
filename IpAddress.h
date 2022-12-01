3797c0537e71c8996d367266dcfeba15d1b54870
yes3421 <karl_heinrich1883@outlook.com>
2022-12-01 01:22:00 +0000

    Added data

--- IpAddress.h
+++ IpAddress.h
@@ -1,3 +1,18 @@
+/*
+ * Actividad Integral sobre el uso de códigos hash
+ *
+ * Actividad 5.2
+ *
+ * Fernando Gabriel Gutiérrez Madrigal A01424790
+ * Omar Adalid Macías Mayorquín A01635795
+ * Paul Enrique Alonso Ramírez A01634608
+ * Programación de estructuras de datos y algoritmos fundamentales
+ * Grupo 614
+ * Equipo 5
+ * Tecnológico de Monterrey, Campus Guadalajara
+ * 30 de noviembre de 2022
+ */
+ 
 #ifndef IPADDRESS_H
 #define IPADDRESS_H
 
